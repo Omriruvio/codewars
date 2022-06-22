ba0a302ec1d0d2293188a4c57e6a1ee91ec89210
Omri Ruvio <omriruvio@gmail.com>
2022-06-22 18:37:57 +0300

    kata: binary addition

---
+++ binary-addition.js
@@ -0,0 +1,6 @@
+// kata - https://www.codewars.com/kata/551f37452ff852b7bd000139
+
+const binaryAddition = (a, b) => (a + b).toString(2);
+
+console.log(binaryAddition(1, 1), '10');
+console.log(binaryAddition(5, 9), '1110');

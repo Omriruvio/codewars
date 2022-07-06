e93793393df42cca2793fed3753bdabaa394f436
Omri Ruvio <omriruvio@gmail.com>
2022-07-06 09:02:42 +0300

    kata: string ends with

---
+++ string-ends-with.js
@@ -0,0 +1,8 @@
+// kata - https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+
+function solution(str, ending) {
+  return !ending ? true : str.slice(-ending.length) === ending;
+}
+
+console.log(solution('abcde', 'cde'), true);
+console.log(solution('abcde', 'abc'), false);

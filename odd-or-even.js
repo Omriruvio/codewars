1aedad9e0ac9a0f2d85c38ab095ba2be12bc3785
Omri Ruvio <omriruvio@gmail.com>
2022-07-07 08:31:06 +0300

    kata: odd or even

---
+++ odd-or-even.js
@@ -0,0 +1,21 @@
+// kata - https://www.codewars.com/kata/5949481f86420f59480000e7
+
+function oddOrEven(array) {
+  return array.reduce((a, b) => a + b, 0) % 2 === 0 ? 'even' : 'odd';
+}
+
+console.log(oddOrEven([0]), 'even');
+console.log(oddOrEven([1]), 'odd');
+console.log(oddOrEven([]), 'even');
+console.log(oddOrEven([0, 1, 5]), 'even');
+console.log(oddOrEven([0, 1, 3]), 'even');
+console.log(oddOrEven([1023, 1, 2]), 'even');
+console.log(oddOrEven([0, -1, -5]), 'even');
+console.log(oddOrEven([0, -1, -3]), 'even');
+console.log(oddOrEven([-1023, 1, -2]), 'even');
+console.log(oddOrEven([0, 1, 2]), 'odd');
+console.log(oddOrEven([0, 1, 4]), 'odd');
+console.log(oddOrEven([1023, 1, 3]), 'odd');
+console.log(oddOrEven([0, -1, 2]), 'odd');
+console.log(oddOrEven([0, 1, -4]), 'odd');
+console.log(oddOrEven([-1023, -1, 3]), 'odd');

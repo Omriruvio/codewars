6aafff5a5461e40cae79c1c8df7b312734c55e2b
Omri Ruvio <omriruvio@gmail.com>
2022-07-08 16:22:45 +0300

    kata: reverse words

---
+++ reverse-words.js
@@ -0,0 +1,13 @@
+// kata - https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/
+
+function reverseWords(str) {
+  return str
+    .split(' ')
+    .map((x) => [...x].reverse().join(''))
+    .join(' ');
+}
+
+console.log(reverseWords('The quick brown fox jumps over the lazy dog.'), 'ehT kciuq nworb xof spmuj revo eht yzal .god');
+console.log(reverseWords('apple'), 'elppa');
+console.log(reverseWords('a b c d'), 'a b c d');
+console.log(reverseWords('double  spaced  words'), 'elbuod  decaps  sdrow');

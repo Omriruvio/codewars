51060c60a7ae0ca7da579c372118604c2464521e
Omri Ruvio <omriruvio@gmail.com>
2022-09-27 00:54:50 +0300

    kata: row weights

---
+++ row-weights.js
@@ -0,0 +1,24 @@
+// kata - https://www.codewars.com/kata/5abd66a5ccfd1130b30000a9
+
+const rowWeights = (arr) => {
+  return arr.reduce(
+    (result, weight, i) => {
+      if ((i + 1) % 2 === 0) result[1] += weight;
+      else result[0] += weight;
+      return result;
+    },
+    [0, 0]
+  );
+};
+
+const assert = require('assert');
+assert.deepEqual(rowWeights([80]), [80, 0]);
+assert.deepEqual(rowWeights([100, 50]), [100, 50]);
+assert.deepEqual(rowWeights([50, 60, 70, 80]), [120, 140]);
+assert.deepEqual(rowWeights([13, 27, 49]), [62, 27]);
+assert.deepEqual(rowWeights([70, 58, 75, 34, 91]), [236, 92]);
+assert.deepEqual(rowWeights([29, 83, 67, 53, 19, 28, 96]), [211, 164]);
+assert.deepEqual(rowWeights([0]), [0, 0]);
+assert.deepEqual(rowWeights([100, 51, 50, 100]), [150, 151]);
+assert.deepEqual(rowWeights([39, 84, 74, 18, 59, 72, 35, 61]), [207, 235]);
+assert.deepEqual(rowWeights([0, 1, 0]), [0, 1]);

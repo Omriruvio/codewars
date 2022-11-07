6bfc458d094e55db21e521de3a1ae5b4645fa351
Omri Ruvio <omriruvio@gmail.com>
2022-11-07 20:46:38 +0200

    kata: sort arrays

---
+++ sort-arrays.js
@@ -0,0 +1,9 @@
+// kata - https://www.codewars.com/kata/51f41fe7e8f176e70d0002b9
+
+const sortme = (names) => {
+  return names.sort((a, b) => {
+    return a.toLowerCase().localeCompare(b.toLowerCase());
+  });
+};
+
+console.log(sortme(['Hello', 'there', 'I', 'am', 'here']), ['am', 'Hello', 'here', 'I', 'there']);
